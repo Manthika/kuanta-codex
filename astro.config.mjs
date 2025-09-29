153f6a4982b9a44960f713cf5ab563577754954e
Manthika <tharumudu@gmail.com>
2025-09-30 12:10:17 +1300

    Changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,4 +8,17 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
+	i18n: {
+		// list your languages
+		locales: ["en", "ko"],
+		// which one is the default
+		defaultLocale: "en",
+		// keep default language at root ("/"), others get prefixed ("/ko")
+		routing: {
+		prefixDefaultLocale: false,      // "/about", "/ko/about"
+		// redirect "/" -> "/en" only when you set prefixDefaultLocale:true
+		// redirectToDefaultLocale: false // (keep default behavior)
+		},
+		// optional: per-locale fallbacks or custom codes/domains later
+	},
 });
